b2c965807368b7eb276c68c26fa872fde1437838
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-12-05 13:43:03 -0300

    Error: Operation isnt working

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -2,14 +2,12 @@ import { Router } from "express";
 
 import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
 
-import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
+import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
-const specificationsRepository = new SpecificationsRepository();
+const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post("/", (request, response) => {
-  return createSpecificationController.handle(request, response)
-})
+specificationsRoutes.post("/", createSpecificationController.handle)
 
 export { specificationsRoutes }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,19 @@
 import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
+import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
+import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 
 // Passa nossa interface ICategoriesRepository e vamos da um nome para chamar a classe
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   // Classe que a gente vai chamar ao chamar o nome
   CategoriesRepository
+)
+
+container.registerSingleton<ISpecificationsRepository>(
+  "SpecificationsRepository",
+  SpecificationsRepository
 )
