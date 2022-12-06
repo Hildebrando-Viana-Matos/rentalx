f6c90ced4739c5c5858cce408ca33ff4967a371d
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-12-06 14:14:15 -0300

    Creating Specifications migrations, but have a error

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,27 +1,23 @@
+import { getRepository, Repository } from "typeorm";
 import { Specification } from "../../entities/Specification";
 import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
-  private specifications: Specification[];
+  private repository: Repository<Specification>;
 
   constructor() {
-    this.specifications = [];
+    this.repository = getRepository(Specification);
   }
 
-  create({ name, description }: ICreateSpecificationDTO): void {
-    const specification = new Specification();
-
-    Object.assign(specification, {
-      name,
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = this.repository.create({
       description,
-      created_at: new Date()
+      name,
     })
-
-    this.specifications.push(specification)
   }
 
-  findByName(name: string): Specification | undefined {
-    const specification = this.specifications.find((specification) => specification.name === name);
+  async findByName(name: string): Promise<Specification | undefined> {
+    const specification = this.repository.findOne({ name });
     return specification;
   }
 }
