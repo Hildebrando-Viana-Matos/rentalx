c76e641ef95134809a4866a49f400c4aa784af2d
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-12-14 15:26:22 -0300

    Creating User Repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,9 +2,11 @@ import { container } from "tsyringe";
 
 import { ICategoriesRepository } from "../../modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificationsRepository";
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
 
 import { CategoriesRepository } from "../../modules/cars/repositories/implementations/CategoriesRepository"
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
 
 // Passa nossa interface ICategoriesRepository e vamos da um nome para chamar a classe
 container.registerSingleton<ICategoriesRepository>(
@@ -16,4 +18,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
+)
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
 )
